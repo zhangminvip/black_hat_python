1fa12811a6b99f4729493abb147d6f7b6719a077
minzhang <minzhanghongkong@gmail.com>
2018-11-08 19:29:03 +0800

    finish run_command func

--- netcat/bphnet.py
+++ netcat/bphnet.py
@@ -130,6 +130,18 @@ def server_loop():
         client_thread.start()
 
 
+def run_command(command):
+
+
+    command = command.rstrip()
+
+    try:
+        output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    except:
+        output = 'Failed to execute command.\r\n'
+
+    return output
+
 
 
 
