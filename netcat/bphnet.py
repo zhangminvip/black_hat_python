74dcc98d1cfebfe80a443524d214de88354384e6
minzhang <minzhanghongkong@gmail.com>
2018-11-08 21:46:06 +0800

    finish client_handler func

--- netcat/bphnet.py
+++ netcat/bphnet.py
@@ -83,6 +83,7 @@ def client_sender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
+        print 'target:%s, port:%s' % (target,int(port))
         client.connect((target, int(port)))
 
         if len(buffer):
@@ -120,8 +121,9 @@ def server_loop():
     if not len(target):
         target = '0.0.0.0'
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((target, port))
+    server.bind((target, int(port)))
     server.listen(5)
+    print 'server ..'
 
     while True:
         client_socket, addr = server.accept()
@@ -145,8 +147,52 @@ def run_command(command):
 
 
 
+
+
+
 def client_handler(client_socket):
-    pass
+    global upload
+    global execute
+    global command
+
+
+    if len(upload_destination):
+
+        file_buffer = ''
+
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            else:
+                file_buffer += data
+
+
+    try:
+        file_descriptor = open(upload_destination, 'wb')
+        file_descriptor.write(file_buffer)
+        file_descriptor.close()
+
+        client_socket.send('Successfully saved file to %s\r\n' % upload_destination)
+    except:
+        client_socket.send('failed to save file to %s\r\n' % upload_destination)
+
+    if len(execute):
+
+        output = run_command(execute)
+        client_socket.send(output)
+
+    if command:
+        while True:
+            client_socket.send('<BHP:#>')
+            cmd_buffer = ''
+            while '\n' not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            response = run_command(cmd_buffer)
+            client_socket.send(response)
+
+
+
 
 
 main()
