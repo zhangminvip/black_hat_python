0b6fa510f78d0809f47dca6ead0be43abfb3fc25
minzhang <minzhanghongkong@gmail.com>
2018-11-09 00:19:12 +0800

    modified format

--- netcat/bphnet.py
+++ netcat/bphnet.py
@@ -14,7 +14,6 @@ target = ''
 upload_destination = ''
 port = 0
 
-
 def usage():
     print 'BHP Net Tool'
     print
@@ -43,7 +42,6 @@ def main():
     global command
     global upload_destination
     global target
-
     if not len(sys.argv[1:]):
         pass
     try:
@@ -52,7 +50,6 @@ def main():
     except getopt.GetoptError as err:
         print str(err)
         usage()
-
     for o, a in opts:
         if o in ('-h', '--help'):
             usage()
@@ -70,7 +67,6 @@ def main():
             port = a
         else:
             assert False, 'Unhandled Option'
-
     if not listen and len(target) and port > 0:
         buffer = sys.stdin.read()
         print'hahaha'
@@ -78,35 +74,25 @@ def main():
     if listen:
         server_loop()
 
-
 def client_sender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         print 'target:%s, port:%s' % (target,int(port))
         client.connect((target, int(port)))
-
         if len(buffer):
             client.send(buffer)
-
         while (True):
-
             recv_len = 1
             response = ''
-
             while recv_len:
-
                 data = client.recv(4096)
                 recv_len = len(data)
                 response += data
                 if recv_len < 4096:
                     break
-
             print response
-
             buffer = raw_input('')
             buffer += '\n'
-
             client.send(buffer)
     except Exception as e:
         print e
@@ -117,14 +103,12 @@ def client_sender(buffer):
 
 def server_loop():
     global target
-
     if not len(target):
         target = '0.0.0.0'
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target, int(port)))
     server.listen(5)
     print 'server ..'
-
     while True:
         client_socket, addr = server.accept()
 
@@ -133,33 +117,22 @@ def server_loop():
 
 
 def run_command(command):
-
-
+    print 'commandbf',command
     command = command.rstrip()
-
+    print 'commandaf',command
     try:
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
     except:
         output = 'Failed to execute command.\r\n'
-
     return output
-
-
-
-
-
-
 
 def client_handler(client_socket):
     global upload
     global execute
     global command
 
-
     if len(upload_destination):
-
         file_buffer = ''
-
         while True:
             data = client_socket.recv(1024)
             if not data:
@@ -167,18 +140,15 @@ def client_handler(client_socket):
             else:
                 file_buffer += data
 
-
     try:
         file_descriptor = open(upload_destination, 'wb')
         file_descriptor.write(file_buffer)
         file_descriptor.close()
-
         client_socket.send('Successfully saved file to %s\r\n' % upload_destination)
     except:
         client_socket.send('failed to save file to %s\r\n' % upload_destination)
 
     if len(execute):
-
         output = run_command(execute)
         client_socket.send(output)
 
@@ -187,12 +157,12 @@ def client_handler(client_socket):
             client_socket.send('<BHP:#>')
             cmd_buffer = ''
             while '\n' not in cmd_buffer:
+                print 'server:recv1'
                 cmd_buffer += client_socket.recv(1024)
+                print 'while:',cmd_buffer
+                print 'server:recv2'
+            print 'h:', cmd_buffer
             response = run_command(cmd_buffer)
             client_socket.send(response)
-
-
-
-
 
 main()
