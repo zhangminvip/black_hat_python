c990611a922c9ebb52c291ffdbb3a3158834ca74
minzhang <minzhanghongkong@gmail.com>
2018-11-09 22:55:36 +0800

    finish server loop func

--- foundation/udp.py
+++ foundation/udp.py
@@ -8,3 +8,12 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client.sendto('ABCD', (target_host, target_port))
 data, addr = client.recvfrom(4096)
 print data
+
+# class Flowler:
+#
+#     def __init__(self):
+#         self.__color = 'Red'
+#
+#
+# rose = Flowler()
+# print rose._Flowler__color
