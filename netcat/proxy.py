9a5b87a59861a6e75d33d0fbdda1569472771232
minzhang <minzhanghongkong@gmail.com>
2018-11-10 02:31:09 +0800

    finish proxy_handler func

--- netcat/proxy.py
+++ netcat/proxy.py
@@ -20,8 +20,8 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
                                         args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
 
-def main():
 
+def main():
     if len(sys.argv[1:]) != 5:
         print 'Usage: ./proxy.py [localhost] [localport] [remotehost] [remoteport] [receive_first]'
         print 'Example: ./proxy.py 127.0.0.1 9000 10.12.132.1 9000 True'
@@ -38,12 +38,36 @@ def main():
     server_loop(local_host, local_port, remote_host, remote_port, receive_first)
 
 
-def proxy_handler(client_socket, remote_host, remote_port,receive_first):
-
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     remote_socket.connect((remote_host, remote_port))
     if receive_first:
         remote_buffer = receive_from(remote_socket)
+        hexdump(remote_buffer)
+        remote_buffer = response_handler(remote_buffer)
+        if len(remote_buffer):
+            print '[<==]Sending %d bytes to localhost.' % len(remote_buffer)
+            client_socket.send(remote_buffer)
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            print '[==>] Received %d bytes form localhost.' % len(local_buffer)
+            hexdump(local_buffer)
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print '[==>] Send to remote'
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print '[<==] Received %d bytes from remote.' % len(remote_buffer)
+            hexdump(remote_buffer)
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print '[<==] Send to localhost'
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print '[*] No more data, Closing connections'
+            break
 
 
 def receive_from(connection):
@@ -59,13 +83,22 @@ def receive_from(connection):
         pass
     return buffer
 
+
 def hexdump(src, length=16):
     result = []
     digits = 4 if isinstance(src, unicode) else 2
     for i in xrange(0, len(src), length):
-        s = src[i:i+length]
+        s = src[i:i + length]
         hexa = b' '.join('%0*X' % (digits, ord(x)) for x in s)
         text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
-        result.append(b'%04X   %-*s %s' % (i, length*(digits + 1), hexa, text))
+        result.append(b'%04X   %-*s %s' % (i, length * (digits + 1), hexa, text))
 
     print b'\n'.join(result)
+
+
+def request_handler(buffer):
+    return buffer
+
+
+def response_handler(buffer):
+    return buffer
